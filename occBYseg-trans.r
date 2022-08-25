ec043b2242b3e303e027e2fe42581d150c98617f
Sarah Herbert <ms.sherbert@gmail.com>
2022-08-25 18:26:54 +1200

    Create occBYseg-trans.r

---
+++ occBYseg-trans.r
@@ -0,0 +1,22 @@
+#--- Transform data into observed presence by transect segment ----#
+
+# set working directory to where your data are
+setwd("D:/R_outputs")
+
+data <- read.csv("All_captures_2012-2022.csv",header=T)
+attach(data)
+
+n.segments <- 10	# no. of segments each transect divided into
+
+count.segment <- 1+trunc((x-0.001)/n.segments) # segment assigned to each observation
+
+# count number found in each segment for each survey for each transect for each year
+segment.counts <- table(Survey,count.segment,Transect,Year)
+# convert to presence-absence
+segment.presence <- 1*(segment.counts > 0)
+
+write(segment.counts,file="segment.counts.txt",3)
+write(segment.presence,file="segment.presence.txt",3)
+
+detach(data)
+rm(list=ls()) #Just to keep your R environment tidy ;)
